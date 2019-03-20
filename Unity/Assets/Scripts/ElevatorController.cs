12d3b5ee2c951ac977dc40f4540d6c1712fd9d97
K6PIR <kellian.cipierre@gmail.com>
2019-03-20 21:28:06 +0100

    Level01 completed

---
+++ Unity/Assets/Scripts/ElevatorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour
+{
+
+    public Transform topPosition;
+
+    public Transform highPosition;
+    public Transform lowPosition;
+
+    public GameObject lever;
+
+    public float moveSpeed;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (lever.GetComponent<Lever_Activation>().leverPosition > 0 && topPosition.position.y < highPosition.position.y)
+        {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveSpeed);
+        } else if (lever.GetComponent<Lever_Activation>().leverPosition < 0 && topPosition.position.y > lowPosition.position.y)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -moveSpeed);
+        } else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+}
