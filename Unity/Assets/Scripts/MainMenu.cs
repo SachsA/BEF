f619a842c5b43bfccdfacfb908730b8212a4b18d
K6PIR <kellian.cipierre@gmail.com>
2019-03-21 01:02:20 +0100

    Level3 designed

--- Unity/Assets/Scripts/MainMenu.cs
+++ Unity/Assets/Scripts/MainMenu.cs
@@ -6,8 +6,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        SceneManager.LoadScene("Level02");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
