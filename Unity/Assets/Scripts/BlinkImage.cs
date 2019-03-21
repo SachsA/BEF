304aad16ea9b56ca9fa4d0aaf7016088b81b7e23
Alexandre Sachs <alexandre.sachs@outlook.fr>
2019-03-21 21:09:52 +0100

    ending scene done

---
+++ Unity/Assets/Scripts/BlinkImage.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BlinkImage : MonoBehaviour
+{
+    private float spriteBlinkingTimer = 1.0f;
+    private float spriteBlinkingMiniDuration = 0.6f;
+    private bool isClicked = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isClicked = true;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+        if (!isClicked)
+            SpriteBlinkingEffect();
+
+    }
+
+    private void SpriteBlinkingEffect()
+    {
+        spriteBlinkingTimer += Time.deltaTime;
+        if (spriteBlinkingTimer >= spriteBlinkingMiniDuration)
+        {
+            spriteBlinkingTimer = 0.0f;
+            if (gameObject.GetComponent<SpriteRenderer>().enabled == true)
+            {
+                gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/FadeInImage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FadeInImage : MonoBehaviour
+{
+    public float delay = 0.0f;
+    public float minimum = 0.0f;
+    public float maximum = 1f;
+    public float duration = 5.0f;
+
+    private float startTime;
+    private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        startTime = Time.time + delay;
+    }
+
+    private void Update()
+    {
+        float t = (Time.time - startTime) / duration;
+        sprite.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(minimum, maximum, t));
+    }
+}
